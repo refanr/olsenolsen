88fbe8be8f32b6ee0a9a6838ca21e84f55bb78bf
Reynir Gunnarsson <reynirg20@ru.is>
2023-03-02 22:32:51 +0000

    trying to implement game play mechanics

--- olsen.py
+++ olsen.py
@@ -1,5 +1,6 @@
 import random
 
+
 def deal(hand1, hand2, deck):
     for i in range(5):
         card = random.choice(deck)
@@ -9,16 +10,12 @@ def deal(hand1, hand2, deck):
         hand2.append(card)
         deck.remove(card)
 
-def draw(hand, deck):
+
+def draw_from_deck(hand, deck):
     card = random.choice(deck)
     hand.append(card)
     deck.remove(card)
-    
-def init_game(table, deck):
-    
-    card = random.choice(deck)
-    table.append(card)
-    deck.remove(card)
+
 
 def is_legal(card, table):
     if card[0] == 8:
@@ -28,42 +25,103 @@ def is_legal(card, table):
     else:
         return False
 
+
 def play_card(card, table):
     if is_legal(card, table):
         table.append(card)
         return True
     else:
         print('Illegal move')
-        
-def no_legal_moves(hand, deck):
+
+
+def no_legal_moves(hand, deck, table):
     for i in range(3):
-        draw(hand, deck)
+        draw_from_deck(hand, deck)
         if is_legal(hand[-1], table):
             play_card(hand[-1], table)
-        
-  
-        
-    
-    
-    
-    
-
-values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
-deck = [(v, s) for v in values for s in suits]
-
-hand1 = []
-hand2 = []
-
-table = []
-
-init_game(table, deck)
-deal(hand1, hand2, deck)
-    
-print(table[-1])
-print(hand1)
-print(hand2)
-print(len(deck))
 
 
+def re_shuffle(deck, table):
+    deck.extend(table)
+    table.clear()
+    draw_from_deck(table, deck)
 
+
+def random_move(hand, deck, table):
+    for card in hand:
+        if is_legal(card, table):
+            play_card(card, table)
+            hand.remove(card)
+            return True
+    no_legal_moves(hand, deck, table)
+
+
+def make_deck():
+    values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+    suits = ['H', 'D', 'C', 'S']
+    deck = [(v, s) for v in values for s in suits]
+    return deck
+
+
+def print_card(card):
+    ret_str = ''
+    if card[1] == 'H':
+        ret_str += '♥'
+    elif card[1] == 'D':
+        ret_str += '♦'
+    elif card[1] == 'C':
+        ret_str += '♣'
+    elif card[1] == 'S':
+        ret_str += '♠'
+    ret_str += card[0]
+    return ret_str
+
+
+def play():
+    deck = make_deck()
+
+    player = []
+    rando = []
+
+    table = []
+
+    draw_from_deck(table, deck)
+    deal(player, rando, deck)
+
+    player_turn = True
+    game_over = False
+
+    while (game_over == False):
+        if player_turn:
+
+            print('Player hand: ', player)
+            print('Table: ' + print_card(table[-1]))
+            print('Enter card to play: ')
+            card = input()
+            if card == 'D':
+                no_legal_moves(player, deck, table)
+                player_turn = False
+                continue
+            card = card.split()
+            card = (card[0], card[1])
+            if play_card(card, table):
+                player.remove(card)
+                player_turn = False
+            else:
+                print('Illegal move')
+        else:
+            print('Rando turn')
+            random_move(rando, deck, table)
+            print('Table: ', print_card(table[-1]))
+            player_turn=True
+
+        if (len(player) == 0):
+            print('Player wins')
+            game_over=True
+        elif (len(rando) == 0):
+            print('Rando wins')
+            game_over=True
+
+
+if __name__ == '__main__':
+    play()
